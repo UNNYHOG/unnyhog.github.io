a3f972ffabc5d7b9b1bd2fb47d397f973dbac7d0
Pavel Ignatov <sunny@unnyhog.com>
2019-06-24 00:10:11 +0300

    update

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -1,7 +1,20 @@
 class GameAnalytics {
 
     sendEvent(name, params) {
+        if (!params)
+            params = {};
+        params.eventCategory = name;
+        ga('send', 'event', params);
+    }
 
+    sendServerError(error, type) {
+        console.error("sendServerError", error);
+
+        this.sendEvent("ServerError", {
+            error: error.code,
+            message: error.message,
+            type: type
+        });
     }
 }
 
