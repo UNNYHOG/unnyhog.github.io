fd3136b46547ed55fcd8a375c0a06f2c6883dd96
Pavel Ignatov <sunny@unnyhog.com>
2020-01-11 02:44:51 +0300

    test

--- myth/index.js
+++ myth/index.js
@@ -20834,7 +20834,7 @@ const BoxType = {
 const MUSIC_STATE = "MUSIC_STATE";
 const SOUNDS_STATE = "SOUNDS_STATE";
 const CURRENT_LANGUAGE = "CURRENT_LANGUAGE";
-const GAME_VERSION = "0.9.35";
+const GAME_VERSION = "0.9.34.1";
 
 console.log("game version: " + GAME_VERSION);
 
@@ -33571,7 +33571,7 @@ let config = {
                 {
                     type: 'scenePlugin',
                     key: 'rexuiplugin',
-                    url: 'plugins/rexuiplugin.min.js',
+                    url: remotePluginPath + 'plugins/rexuiplugin.min.js',
                     sceneKey: 'rexUI'
                 }
             ]
